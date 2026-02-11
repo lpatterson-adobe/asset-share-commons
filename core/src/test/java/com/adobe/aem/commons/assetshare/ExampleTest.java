64792d7a102581b4263d6b41281f144af785678f
lpatterson <lpatterson@adobe.com>
2026-02-11 09:23:02 -0500

    c

---
+++ core/src/test/java/com/adobe/aem/commons/assetshare/ExampleTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+    @Test
+    void testAddition() {
+        assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    void testSubtraction() {
+        assertEquals(0, 1 - 1);
+    }
+}
